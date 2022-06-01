307a839ca3f3de3535b55b757e97492984f1486e
Maksim1509 <maaa23@mail.ru>
2022-06-01 14:54:03 +0400

    refactor: add alt to button from new tasks according to rule 1.2 html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="button button_delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt="Remove";
     deleteButtonImg.className="button__img";
     deleteButton.appendChild(deleteButtonImg);
 
